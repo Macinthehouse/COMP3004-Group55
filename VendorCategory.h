0f44524785483cb163cf853f969e9d5ba23f6384
Hussi767 <124533350+Hussi767@users.noreply.github.com>
2026-03-12 17:50:26 -0400

    Refactor VendorCategory.h by removing comments
    Removed comments for clarity and streamlined code.

--- VendorCategory.h
+++ VendorCategory.h
@@ -1,14 +1,12 @@
 #ifndef VENDORCATEGORY_H
 #define VENDORCATEGORY_H
 
-// --------------------------------------------------
 // VendorCategory Enum
-// --------------------------------------------------
+
 // Represents the type of vendor in the system.
 // Used for:
 // - Stall capacity rules
 // - Waitlist separation
-// --------------------------------------------------
 
 enum class VendorCategory {
     FOOD,
