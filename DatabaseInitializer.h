a35f99bc1b24f69815a2f9f3f64bbcc8d22adad8
Hussi767 <124533350+Hussi767@users.noreply.github.com>
2026-03-30 20:30:22 -0400

    Create DatabaseInitializer.h

---
+++ DatabaseInitializer.h
@@ -0,0 +1,19 @@
+#ifndef DATABASEINITIALIZER_H
+#define DATABASEINITIALIZER_H
+
+class DatabaseInitializer
+{
+public:
+    bool initialize();
+
+private:
+    bool createTables();
+    bool seedDefaultDataIfNeeded();
+
+    bool isTableEmpty(const char* tableName);
+
+    bool seedUsers();
+    bool seedMarketDates();
+    bool seedComplianceDocuments();
+};
+#endif // DATABASEINITIALIZER_H
