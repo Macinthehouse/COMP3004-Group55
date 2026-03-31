c5f9158848e9c2db422ed65e9b98382c46c8d4a7
Hussi767 <124533350+Hussi767@users.noreply.github.com>
2026-03-30 20:42:50 -0400

    Create UserRepository.h

---
+++ UserRepository.h
@@ -0,0 +1,19 @@
+#ifndef USERREPOSITORY_H
+#define USERREPOSITORY_H
+
+#include <memory>
+#include <string>
+#include <vector>
+
+class User;
+
+class UserRepository
+{
+public:
+    std::unique_ptr<User> findById(const std::string& id);
+    std::unique_ptr<User> findByName(const std::string& name);
+
+    std::vector<std::unique_ptr<User>> loadAllUsers();
+};
+
+#endif // USERREPOSITORY_H
