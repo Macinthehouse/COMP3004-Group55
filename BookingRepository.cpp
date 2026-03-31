e552b99b0159bb92c88c11321aa8afe639931976
Hussi767 <124533350+Hussi767@users.noreply.github.com>
2026-03-31 18:08:49 -0400

    Create BookingRepository.cpp

---
+++ BookingRepository.cpp
@@ -0,0 +1,47 @@
+#include "BookingRepository.h"
+#include "DatabaseManager.h"
+#include "VendorCategory.h"
+
+#include <QSqlQuery>
+#include <QSqlError>
+#include <QVariant>
+#include <QDebug>
+
+namespace
+{
+    VendorCategory parseVendorCategory(const QString& categoryText)
+    {
+        if (categoryText.toUpper() == "ARTISAN") {
+            return VendorCategory::ARTISAN;
+        }
+        return VendorCategory::FOOD;
+    }
+}
+
+std::vector<Booking> BookingRepository::loadAllBookings()
+{
+    std::vector<Booking> bookings;
+
+    QSqlQuery query(DatabaseManager::instance().database());
+    query.prepare(
+        "SELECT b.vendor_id, b.market_date_id, u.vendor_category "
+        "FROM bookings b "
+        "JOIN users u ON b.vendor_id = u.id "
+        "ORDER BY b.id"
+    );
+
+    if (!query.exec()) {
+        qDebug() << "BookingRepository::loadAllBookings failed:" << query.lastError().text();
+        return bookings;
+    }
+
+    while (query.next()) {
+        bookings.emplace_back(
+            query.value("vendor_id").toString().toStdString(),
+            query.value("market_date_id").toString().toStdString(),
+            parseVendorCategory(query.value("vendor_category").toString())
+        );
+    }
+
+    return bookings;
+}
