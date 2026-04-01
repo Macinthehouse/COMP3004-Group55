ccf523ae2a82bde90a5a79a438bd459a4ce25484
Hussi767 <124533350+Hussi767@users.noreply.github.com>
2026-04-01 17:11:31 -0400

    Update BookingRepository.h

--- BookingRepository.h
+++ BookingRepository.h
@@ -1,6 +1,7 @@
 #ifndef BOOKINGREPOSITORY_H
 #define BOOKINGREPOSITORY_H
 
+#include <string>
 #include <vector>
 
 #include "Booking.h"
@@ -9,6 +10,11 @@ class BookingRepository
 {
 public:
     std::vector<Booking> loadAllBookings();
+
+    bool createBooking(const Booking& booking);
+
+    bool removeBooking(const std::string& vendorId,
+                       const std::string& marketDateId);
 };
 
 #endif // BOOKINGREPOSITORY_H
